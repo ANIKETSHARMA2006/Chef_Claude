b73a1b29f8f83a3b28ae6b6e2c3c59730297520a
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-03-21 19:44:44 +0530

    Fix: corrected API handling and improved components

---
+++ Clade_Chef/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './Components/Header'
+import Labal from './Components/Labal'
+function App() {
+
+
+  return (
+    <>
+    <Header />
+    <Labal />
+    </>
+  )
+}
+
+export default App
