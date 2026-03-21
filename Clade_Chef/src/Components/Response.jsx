513e061564012fda688ae6d3800eba4178f99b21
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-03-21 20:31:34 +0530

    first commit

--- Clade_Chef/src/Components/Response.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Response = ({ recipe }) => (
-  <div className='response-output'>{recipe || 'Add ingredients and generate a recipe to see it here.'}</div>
-)
-
-export default Response
